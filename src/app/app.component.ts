6dd84e3e7926dddb180df5169231f0173adfcbb4
Daniel Penelva <d4n.andrade@gmail.com>
2025-11-11 01:30:04 -0300

    feat: add um modo escuro (dark mode) + alterando index.html, styles.css, app.component .css, .html e .ts, header.component.css
    Esse modo escuro vai ser ativado por uma classe no <body class="dark-mode">.
    O botão vai flutuar no canto superior direito da tela, sobre o header. Tem formato circular com animação ao passar o mouse com cores que troca automaticamente quando o modo escuro é ativado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'loja-virtual';
+
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
 }
