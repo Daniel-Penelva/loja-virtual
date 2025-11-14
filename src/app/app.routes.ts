fc8855846367b570c09babef2a2b76d23235d67b
Daniel Penelva <d4n.andrade@gmail.com>
2025-11-14 00:27:57 -0300

    feat: (funcionalidade criando uma página sobre moderna e estilizada) criando componente Sobre + add rota + add link de navegação de rota no footer + implementando template SobreComponent .html e estilizando CSS SobreComponent .css
    Comando para criar o componente Sobre dentro da pasta pages:
    ng g c pages/sobre
    
    Adicionando rota "sobre" no app.routes. Add redirectTo para rota padrão e path: ** para rota de erro 404.
    
    Inserir o link de navegação no menu utilizando routerLink no FooterComponent .html
    
    Add <router-outlet> no app.component.html para permitir abrir  as páginas.
    
    Foi criado uma página com layout moderno com seção (SobreComponent .html) e customizada com css (SobreComponent .css) moderno e responsivo integrado com o modo escuro e claro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'sobre', component: SobreComponent },
+    { path: '', redirectTo: 'sobre', pathMatch: 'full' }, // rota padrão
+    { path: '**', redirectTo: 'sobre' }, // caso entra em algo inválido - rota 404
+];
