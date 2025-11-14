b70b420b71dfd230cacf82bf8a0ee89cafd4dd02
Daniel Penelva <d4n.andrade@gmail.com>
2025-11-14 01:06:24 -0300

    feat: (funcionalidade: criando o componente home, e desenvolvendo seu template e sua estilização e add rotas) Criando componente home e implementando ele + alterando app.routes.ts, footerComponent.html e style.css
    Criado o componente home - comando: ng g c pages/home
    
    Foi add os links de navegação routerLink no nav do links rápidos no Footer
    
    Foi add as rotas do Home no app.routes.ts
    
    HomeComponent .html template feito com seção definindo duas divs uma para um banner principal e a outra uma categoria com três cards.
    
    HomeComponent .css foi usado gradiente (linear-gradient) para banner principal. Na seção categorias foi feito em grid com animações com delays que aparecem de 1 em 1.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: 'sobre', component: SobreComponent },
-    { path: '', redirectTo: 'sobre', pathMatch: 'full' }, // rota padrão
-    { path: '**', redirectTo: 'sobre' }, // caso entra em algo inválido - rota 404
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, // rota padrão
+    { path: '**', redirectTo: 'home' }, // caso entra em algo inválido - rota 404
 ];
